5d6281b1b099a8c90c2ec370cdb16f729ba8897f
wildwalks <matt@wildwalks.com>
2023-06-08 17:37:19 +1000

    add power to schema

--- src/main/java/org/openmaptiles/layers/Power.java
+++ src/main/java/org/openmaptiles/layers/Power.java
@@ -1,12 +1,17 @@
 package org.openmaptiles.layers;
 import com.onthegomap.planetiler.FeatureCollector;
+import com.onthegomap.planetiler.config.PlanetilerConfig;
 import com.onthegomap.planetiler.reader.SourceFeature;
+import com.onthegomap.planetiler.stats.Stats;
+import com.onthegomap.planetiler.util.Translations;
 import org.openmaptiles.Layer;
 import org.openmaptiles.OpenMapTilesProfile;
 
 public class Power implements Layer, OpenMapTilesProfile.OsmAllProcessor {
 
   private static final String LAYER_NAME = "power";
+
+  public Power(Translations translations, PlanetilerConfig config, Stats stats) {}
 
   @Override
   public String name() {
