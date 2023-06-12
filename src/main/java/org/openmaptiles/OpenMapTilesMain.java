227ac2e79ebf45e3fd70d3d6d5a3fe95288e058f
wildwalks <matt@wildwalks.com>
2023-06-12 19:34:01 +1000

    shp change

--- src/main/java/org/openmaptiles/OpenMapTilesMain.java
+++ src/main/java/org/openmaptiles/OpenMapTilesMain.java
@@ -52,8 +52,8 @@ public class OpenMapTilesMain {
       // override with --mbtiles=... argument or MBTILES=... env var or mbtiles=... in a config file
       .setOutput("mbtiles", dataDir.resolve("output.mbtiles"))
       .addShapefileSource(OpenMapTilesProfile.CONTOUR_SOURCE,
-        sourcesDir.resolve("au_contours_AX_39_LINES.zip"),
-        "https://osmdata.openstreetmap.de/download/au_contours_AX_39_LINES.zip")
+        sourcesDir.resolve("contours.zip"),
+        "https://osmdata.openstreetmap.de/download/contours.zip")
       .run();
   }
 }
